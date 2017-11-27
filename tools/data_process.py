cffec7777db8862c226fbc52f721b73a83beb343
walter_wu090 <walter.wu090@gmail.com>
2017-11-26 22:28:01 -0500

    Modify to use first column as index

--- tools/data_process.py
+++ tools/data_process.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from keras.preprocessing.sequence import pad_sequences
 
 
 def read_data(csv_file):
-    df = pd.read_csv(csv_file)
+    df = pd.read_csv(csv_file, index_col=0)
     one_hot = pd.get_dummies(df['author'])
     df = df.drop('author', axis=1)
     df = df.join(one_hot)
